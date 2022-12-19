b72f121ec40cad46d37d68b9b379944f0d275aac
unknown <pavithrasivanoli@gmail.com>
2022-12-19 09:41:44 +0530

    updated the files

---
+++ js/new.js
@@ -0,0 +1,117 @@
+/*var d = new Date();
+d.setDate(25);
+d.getDate();
+document.write(d);
+document.write("<br>");*/
+
+/*var d = new Date();
+d.getMonth();
+//d.setMonth(0);
+document.write(d);
+document.write("<br>");*/
+
+/*const my = new Date("5-12-2022, 04:30:55");
+const a= my.getFullYear();
+document.write(a);
+document.write("<br>");
+
+const b= my.getMonth();
+document.write(b);
+document.write("<br>");
+
+const c= my.getDate();
+document.write(c);
+document.write("<br>");
+
+const d= my.getDay();
+document.write(d);
+document.write("<br>");
+
+const e= my.getHours();
+document.write(e);
+document.write("<br>");
+
+const f= my.getMinutes();
+document.write(f);
+document.write("<br>");
+
+const g= my.getSeconds();
+document.write(g);
+document.write("<br>");
+const i= my.getTime();
+document.write(i);
+document.write("<br>");
+const h= my.getMilliseconds();
+document.write(h);
+document.write("<br>");*/
+
+
+/*var d=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+var a=new Date();
+var y=d[a.getDay(5)];
+document.write(y);*/
+
+
+
+/*var d=new Date();
+var y=d.setMonth(5);
+document.write(d);*/
+
+/*var d=new Date();
+d.getMonth();
+document.write(d);*/
+/*var d=new Date();
+var s=d.getFullYear();
+document.write(s);
+
+var a=10;
+b=20;
+console.log(a+b)
+//c=a+b
+console.log(c);*/
+
+var s=new Date();
+s.setDate(23);
+document.write(s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
